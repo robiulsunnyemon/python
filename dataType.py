45d79439cb4f3c95cce4d75dbf2fa88c36916065
robiulsunnyemon <robiulsunyemon@gmail.com>
2024-12-10 18:30:03 +0600

    add datatype.py

---
+++ dataType.py
@@ -0,0 +1,23 @@
+Greeting="Good Morning"; #string type
+print(type(Greeting));
+
+number= 56777; #integer type
+print(type(number));
+
+floatNumber=20.22; #floating type
+print(type(floatNumber));
+
+complexNumber=3+2j; #complex type
+print(type(complexNumber));
+
+Fruits=["Mango","Banana","Cherry"]; #list type
+print(type(Fruits));
+
+Animals=("Cow","Cat","Ant");
+print(type(Animals)); #tuple type
+
+Departments={"CSE","EEE","SWE"} #set type
+print(type(Departments));
+
+employe={"name":"Emon","age":22}; #dictionary
+print(type(employe));
